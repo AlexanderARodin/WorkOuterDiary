22c3abd9609a9777f3e95d6aece470265cdbdfe4
AlexanderARodin <rodin_aa@inbox.ru>
2022-11-04 23:20:42 +0300

    Initial Commit

---
+++ Shared/WorkOuterDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkOuterDiaryApp.swift
+//  Shared
+//
+//  Created by the Dragon on 04.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkOuterDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
